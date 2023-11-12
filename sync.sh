d9f7f81c21eb12683642d71411da2f0b52ef2f90
Code Mountains <thecodemountains@gmail.com>
2023-11-12 01:29:59 -0600

    fruitfall nodejs nginx reverse proxy conf

---
+++ sync.sh
@@ -0,0 +1,4 @@
+mkdir -p /home/sysadmin/Documents/code/js/physics-based-web-game-javascript-html5-1913/etc/nginx/sites-enabled
+sudo cp /etc/nginx/sites-enabled/fruitfall.conf /home/sysadmin/Documents/code/js/physics-based-web-game-javascript-html5-1913/etc/nginx/sites-enabled/fruitfall.conf 
+
+sudo chown -R sysadmin:sysadmin /home/sysadmin/Documents/code/js/physics-based-web-game-javascript-html5-1913
